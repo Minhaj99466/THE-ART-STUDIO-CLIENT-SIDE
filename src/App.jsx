ae18d332987a092497f2d020cfd0aaa8f76fbd13
MINHAJ MM <minhaj99466@gmail.com>
2023-11-16 20:16:31 +0530

    hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
 import UserRoute from "./routes/userRoute/userRoute"
-import ArtistRoute from "./routes/artistRoute/artistRoute"
+import ArtistRoute from "./routes/artistRoute/ArtistRoute"
 import AdminRoute from "./routes/adminRoute/adminRoute"
 import './App.css'
 import ChatUserProvider from "./components/userComponents/ChatComponents/components/Context/ChatProvider"
