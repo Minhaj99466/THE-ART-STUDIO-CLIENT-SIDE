bd482d4068b4b85ab0ad9cbfdf4dfbe8914e0f36
MINHAJ MM <minhaj99466@gmail.com>
2023-11-30 15:39:41 +0530

    adminLoading

--- src/utils/adminRequest.js
+++ src/utils/adminRequest.js
@@ -1,33 +1,32 @@
-import axios from 'axios'
-import { GenerateError } from '../toast/toast'
+import axios from "axios";
+import { GenerateError } from "../toast/toast";
 
-
-const baseUrl = import.meta.env.VITE_ADMIN_URL
+const baseUrl = import.meta.env.VITE_ADMIN_URL;
 
 const axiosInterceptorInstance = axios.create({
-    baseURL: baseUrl
-})
-axiosInterceptorInstance.interceptors.request.use((req)=> {
-    if(localStorage.getItem("AdminToken")){
-        req.headers.Authorization = "Bearer " + localStorage.getItem("AdminToken")
-    }
-    return req
-})
-
-
+  baseURL: baseUrl,
+});
+axiosInterceptorInstance.interceptors.request.use((req) => {
+  if (localStorage.getItem("AdminToken")) {
+    req.headers.Authorization = "Bearer " + localStorage.getItem("AdminToken");
+  }
+  return req;
+});
 
 axiosInterceptorInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error.response && error.response.status === 400) {
-        GenerateError(error.response.data.message);
-      }
-     
-      return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 400) {
+      GenerateError(error.response.data.message);
     }
-  );
+    if (error.response && error.response.status === 500) {
+      GenerateError(error.response.data.message);
+      localStorage.removeItem("AdminToken");
+      window.location = "/admin/login";
+    }
 
+    return Promise.reject(error);
+  }
+);
 
-
-
-export default axiosInterceptorInstance
+export default axiosInterceptorInstance;
